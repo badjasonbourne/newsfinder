9c25688b835387584cc0a780139522f814fb039a
badjasonbourne <badjasonbourne@gmail.com>
2024-12-20 18:27:27 +0800

    Update .gitignore to include markdown files; enhance database connection with user authorization and add INSERT operation test

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -9,11 +9,15 @@ load_dotenv()
 # 获取数据库URL
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# 授权用户ID
+AUTH_UID = "c40899b1-7ea9-41d6-bd4c-3371a4ce564a"
+
 def get_db():
     try:
         conn = psycopg2.connect(
             DATABASE_URL,
-            cursor_factory=RealDictCursor
+            cursor_factory=RealDictCursor,
+            options=f"-c role=authenticated -c request.jwt.claims={{\"sub\":\"{AUTH_UID}\"}}"
         )
         return conn
     except Exception as error:
